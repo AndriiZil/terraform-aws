612df533bb8fc457d1976c5b55aa062647b65aa0
Andrii Zilnyk <andrii.zilnyk@gmail.com>
2020-12-29 18:56:16 +0200

    added new lesson

---
+++ lesson_20/Layer-2-Servers/main.tf
@@ -0,0 +1,47 @@
+provider "aws" {
+    region = "eu-central-1"
+}
+
+data "terraform_remote_state" "network" {
+    backend = "s3"
+    config = {
+        bucket = "andrii-zilyk-project-terraform-state"
+        key = "dev/network/terraform.tfstate"
+        region = "eu-central-1"
+    }
+}
+
+resource "aws_security_group" "my_webserver" {
+    name = "WebServer Security Group"
+    vpc_id = data.terraform_remote_state.network.outputs.vpc_id
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = [data.terraform_remote_state.network.outputs.vpc_cidr]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "web-server-sg"
+        Owner = "Zilnyk Andrii"
+    }
+}
+
+output "network_details" {
+    value = data.terraform_remote_state.network
+}
