db6c15c813226689360b924099839149019bbe1b
Andrii Zilnyk <andrii.zilnyk@gmail.com>
2020-12-30 17:17:37 +0200

    added new modules

--- lesson_20/Layer-2-Servers/main.tf
+++ lesson_20/Layer-2-Servers/main.tf
@@ -2,12 +2,54 @@ provider "aws" {
     region = "eu-central-1"
 }
 
+terraform {
+    backend "s3" {
+        bucket = "andrii-zilyk-project-terraform-state"
+        key = "dev/servers/terraform.tfstate"
+        region = "eu-central-1"
+    }
+}
+
+# =========================================================
+
 data "terraform_remote_state" "network" {
     backend = "s3"
     config = {
         bucket = "andrii-zilyk-project-terraform-state"
         key = "dev/network/terraform.tfstate"
         region = "eu-central-1"
+    }
+}
+
+# =========================================================
+
+data "aws_ami" "latest_amazon_linux" {
+    owners = ["amazon"]
+    most_recent = true
+    filter {
+        name = "name"
+        values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    }
+}
+
+# =========================================================
+
+resource "aws_instance" "web_server" {
+    ami = data.aws_ami.latest_amazon_linux.id
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.my_webserver.id]
+    subnet_id = data.terraform_remote_state.network.outputs.public_subnet_ids[0]
+    user_data = <<EOF
+#!/bin/bash
+yum -y update
+yum -y install httpd
+myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+echo "<h2>WebServer with IP: $myip</h2><br>Build by Terraform with Remote State" > /var/www/html/index.html
+sudo service httpd start
+chkconfig httpd on
+EOF
+    tags = {
+        Name = "WebServer"
     }
 }
 
@@ -42,6 +84,16 @@ resource "aws_security_group" "my_webserver" {
     }
 }
 
+# ===============================================
+
 output "network_details" {
     value = data.terraform_remote_state.network
+}
+
+output "web_sg_id" {
+    value = aws_security_group.my_webserver.id
+}
+
+output "web_server_public_ip" {
+    value = aws_instance.web_server.public_ip
 }
